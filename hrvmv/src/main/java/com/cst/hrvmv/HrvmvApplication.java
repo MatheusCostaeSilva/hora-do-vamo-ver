3946c6d30810b0c6e5edf4f498271630353d80c7
unknown <matheuscostaesilva12@gmail.com>
2026-03-04 20:03:54 -0300

    primeiro commit

---
+++ hrvmv/src/main/java/com/cst/hrvmv/HrvmvApplication.java
@@ -0,0 +1,13 @@
+package com.cst.hrvmv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrvmvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrvmvApplication.class, args);
+	}
+
+}
